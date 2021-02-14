7f02eda9e595d58c898e1f307d20fdbfccfa07ac
David Llewellyn-Jones <david@flypig.co.uk>
2021-02-14 20:10:47 +0000

    Support testResult process flow
    Adds the following additional external REST endpoint, needed to
    simulate the testResult process flow:
    
    /version/v1/testresult
    
    Using the registrationToken 7f86c6fd-5c66-4003-abb1-4eee36680c9b will
    return a test result of 1 (negative) 10% of the time, while using
    b3586b13-280f-4a6d-8b54-5fbf8fb8d550 will return a test result of 2
    (positive) 10% of the time.
    
    For the remainder of the responses a test result of 3 (invalid) is
    returned. If any other registrationToken is used a 400 response is
    returned.

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 'use strict';
 
+// TANS accepted by /version/v1/tan/verify
 const validTans = [
   "edc07f08-a1aa-11ea-bb37-0242ac130002"
 ];
 
+// TeleTANs accepted by /version/v1/registrationToken
 const teleTanToRegToken = {
   "R3ZNUEV9JA": "7f86c6fd-5c66-4003-abb1-4eee36680c9b",
   "CG4Z5A9CY9": "7f86c6fd-5c66-4003-abb1-4eee36680c9b"
 };
 
+// SHA-256 hashed GUIDs accepted by /version/v1/registrationToken
 const guidsToRegToken = {
   "f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b": "7f86c6fd-5c66-4003-abb1-4eee36680c9b",
   "0199effab87800689c15c08e234db54f088cc365132ffc230e882b82cd3ecf95": "b3586b13-280f-4a6d-8b54-5fbf8fb8d550"
 };
 
+// Registration tokens accepted by /version/v1/tan
 const regTokenToTan = {
   "7f86c6fd-5c66-4003-abb1-4eee36680c9b": "edc07f08-a1aa-11ea-bb37-0242ac130002",
   "b3586b13-280f-4a6d-8b54-5fbf8fb8d550": "edc07f08-a1aa-11ea-bb37-0242ac130002"
 };
+
+// Registration tokens accepted by /version/v1/testresult
+const regTokenToTestResult = {
+    "7f86c6fd-5c66-4003-abb1-4eee36680c9b": {testResult: 1, chance: 0.1},
+    "b3586b13-280f-4a6d-8b54-5fbf8fb8d550": {testResult: 2, chance: 0.1}
+}
 
 const server_port = process.env.PORT || 8004;
 const server_host = process.env.IP || '0.0.0.0';
@@ -89,7 +99,7 @@ app.post('/version/v1/registrationToken', function (req, res) {
 });
 
 /**
- * Generates a TAN on input of Registration Token. With the TAN one can submit his Diagnosis keys
+ * Generates a TAN on input of Registration Token. With the TAN one can submit their Diagnosis keys
  * /version/v1/tan
  *
  * Request
@@ -109,6 +119,37 @@ app.post('/version/v1/tan', function (req, res) {
   if (regTokenToTan[req.body.registrationToken] !== undefined) {
     res.status(201);
     res.json({ tan: regTokenToTan[req.body.registrationToken] });
+  } else {
+    res.status(400).send();
+  }
+});
+
+/**
+ * Return test result diagnosis
+ * /version/v1/testresult
+ *
+ * Request
+ * Headers: "cwa-fake: 0"
+ * Content-type: application/json
+ * Body: {"registrationToken", <TOKEN>}
+ *   <TOKEN> a registration token
+ *
+ * Response
+ * HTTP 201 if successful. Otherwise HTTP 400.
+ * Content-type: application/json
+ * Body: {"testResult":<int>}
+ *   <int> test regTokenToTestResult; 1 = Negative; 2 = Positive; 3 = Invalid
+ */
+app.post('/version/v1/testresult', function (req, res) {
+  console.log(req.body);
+  if (regTokenToTestResult[req.body.registrationToken] !== undefined) {
+    res.status(201);
+    var random = Math.random();
+    if (random < regTokenToTestResult[req.body.registrationToken].chance) {
+        res.json({ testResult: regTokenToTestResult[req.body.registrationToken].testResult });
+    } else {
+      res.json({ testResult: 3 });
+    }
   } else {
     res.status(400).send();
   }
